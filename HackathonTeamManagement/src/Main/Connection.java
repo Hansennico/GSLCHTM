e9e74b55d3938b1c9969efa4e19a16ddca808c77
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2023-12-07 21:10:20 +0700

    Update Connection.java

--- HackathonTeamManagement/src/Main/Connection.java
+++ HackathonTeamManagement/src/Main/Connection.java
@@ -7,7 +7,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Connection {
 	public static void read(String csvpath) {
