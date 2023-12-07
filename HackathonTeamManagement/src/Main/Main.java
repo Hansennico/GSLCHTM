a9fefe62cd6425c10b2ae8b53eb87f7af9ebfe09
Hansennico <125004991+Hansennico@users.noreply.github.com>
2023-12-07 21:21:55 +0700

    Update Main.java

--- HackathonTeamManagement/src/Main/Main.java
+++ HackathonTeamManagement/src/Main/Main.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
 package Main;
+
+import java.util.Scanner;
 
 public class Main {
 
@@ -9,6 +10,33 @@ public class Main {
 		Connection teams = new Connection();
 		teams.read("/home/hansen/Documents/OOP/GSLCHTM/Database/teams.csv");
 		teams.read("/home/hansen/Documents/OOP/GSLCHTM/Database/user.csv");
+		
+		Scanner input = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("1. Menu Utama");
+			System.out.println("2. Insert Data");
+			System.out.println("3. Show");
+			System.out.println("4. exit");
+			System.out.print(">> ");
+			int menu = input.nextInt();
+			switch(menu) {
+				case 1:
+					break;
+				case 2:
+					
+					break;
+				case 3:
+					break;
+				case 4:
+					System.exit(0);
+					break;
+				default:
+					break;
+			}
+			
+			
+		}
 		
 	}
 }
