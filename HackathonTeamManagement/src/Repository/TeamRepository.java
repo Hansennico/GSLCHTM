1de1e3b59076c20edd16c7a305dcb0a1ec2fc517
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2023-12-07 20:58:58 +0700

    Update TeamRepository.java

--- HackathonTeamManagement/src/Repository/TeamRepository.java
+++ HackathonTeamManagement/src/Repository/TeamRepository.java
@@ -1,5 +1,19 @@
 package Repository;
 
 public class TeamRepository implements Repository {
+@Override
+    public ArrayList<Team> find(String column, String[] conditions, Boolean join, String joinTable, Connection conn) {
+        // implementation goes here
+    }
 
+    @Override
+    public Team findOne(String column, String[] conditions, Boolean join, String joinTable, Connection conn) {
+        // implementation goes here
+    }
+
+    @Override
+    public Team insert(String[] data, Connection conn) {
+        // implementation goes here
+    }
+}
 }
