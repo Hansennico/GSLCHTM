7fff51a37833ad4e4c4ce52a8a505166edc842c0
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2023-12-07 21:08:05 +0700

    Update UserRepository.java

--- HackathonTeamManagement/src/Repository/UserRepository.java
+++ HackathonTeamManagement/src/Repository/UserRepository.java
@@ -3,17 +3,17 @@ package Repository;
 public class UserRepository implements Repository{
 @Override
     public ArrayList<User> find(String column, String[] conditions, Boolean join, String joinTable, Connection conn) {
-        // implementation goes here
+       
     }
 
     @Override
     public User findOne(String column, String[] conditions, Boolean join, String joinTable, Connection conn) {
-        // implementation goes here
+    
     }
 
     @Override
     public User insert(String[] data, Connection conn) {
-        // implementation goes here
+   
     }
 }
 }
