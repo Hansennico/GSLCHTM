e80f6b1a3d81f7d349ca3fc366c0f257083352d2
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2023-12-07 21:03:29 +0700

    Update Repository.java

--- HackathonTeamManagement/src/Repository/Repository.java
+++ HackathonTeamManagement/src/Repository/Repository.java
@@ -4,5 +4,5 @@ public class Repository {
 ArrayList<Model> find(String column, String[] conditions, Boolean join, String joinTable, Connection conn);
     Model findOne(String column, String[] conditions, Boolean join, String joinTable, Connection conn);
     Model insert(String[] data, Connection conn);
-}
+    }
 }
