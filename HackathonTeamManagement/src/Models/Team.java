bfa6caa72797bdbe47ca38ff9b217bcacb22d69c
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2023-12-07 22:28:17 +0700

    Update Team.java

--- HackathonTeamManagement/src/Models/Team.java
+++ HackathonTeamManagement/src/Models/Team.java
@@ -1,5 +1,9 @@
 package Models;
 
 public class Team extends Model{
+private String TeamID;
+public Model(String UserName, String TeamName){
+  super(UserName, TeamName);
 
+  this.TeamID = TeamID;
 }
