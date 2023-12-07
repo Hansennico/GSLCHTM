8ac268460847229e2235602aded7ee83d6987d7f
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2023-12-07 20:55:09 +0700

    Update Model.java

--- HackathonTeamManagement/src/Models/Model.java
+++ HackathonTeamManagement/src/Models/Model.java
@@ -1,5 +1,26 @@
+import java.util.List;
+import java.util.Map;
 package Models;
 
 public class Model {
-	
+
+
+public class CSVQueryFacade {
+    private CSVReader reader;
+    private CSVWriter writer;
+
+    public CSVQueryFacade(String filePath) {
+        this.reader = new CSVReader(filePath);
+        this.writer = new CSVWriter(filePath);
+    }
+
+    public List<Map<String, String>> query(String column, String value) {
+        return reader.readRecords(column, value);
+    }
+
+    public void insert(Map<String, String> record) {
+        writer.writeRecord(record);
+    }
+}
+
 }
