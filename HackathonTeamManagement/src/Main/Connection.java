18ced638e04ef0cfb45e0326f3369f6704254398
Hansennico <nicolashansen1508040@gmail.com>
2023-12-07 20:35:06 +0700

    V1
    added connection

---
+++ HackathonTeamManagement/src/Main/Connection.java
@@ -0,0 +1,39 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection {
+	public static void read(String csvpath) {
+        String filePath = csvpath;
+
+        List<String[]> records = new ArrayList<>();
+        try {
+            Path path = FileSystems.getDefault().getPath(filePath);
+            BufferedReader reader = new BufferedReader(new FileReader(path.toString()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                records.add(values);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Menampilkan hasil pembacaan file CSV
+        for (String[] record : records) {
+            for (String value : record) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- HackathonTeamManagement/src/Main/Main.java
+++ HackathonTeamManagement/src/Main/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
+		Connection teams = new Connection();
+		teams.read("/home/hansen/Documents/OOP/GSLCHTM/Database/teams.csv");
+		teams.read("/home/hansen/Documents/OOP/GSLCHTM/Database/user.csv");
+		
 	}
-
 }
